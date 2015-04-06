3178e032ceb6aa51f32078e02106d94c89d9cb29
Hesham ALMatary <heshamelmatary@gmail.com>
2015-04-06 12:29:19 +0100

    Modify some errors due to ARM files copying

--- include/arch/riscv/arch/machine/hardware.h
+++ include/arch/riscv/arch/machine/hardware.h
@@ -17,4 +17,52 @@
 
 /* Include cache, MMU related functions */
 
-#endif
+#ifndef __ASSEMBLER__
+
+#include <arch/types.h>
+
+enum vm_fault_type {
+    RISCVDataAbort = 0,
+    RISCVPrefetchAbort = 1 
+};
+typedef uint32_t vm_fault_type_t;
+
+enum vm_page_size {
+    RISCVSmallPage,
+    RISCVLargePage,
+    RISCVSection,
+    RISCVSuperSection
+};
+typedef uint32_t vm_page_size_t;
+
+enum frameSizeConstants {
+    RISCVSmallPageBits    = 12, 
+    RISCVLargePageBits    = 16, 
+    RISCVSectionBits      = 20, 
+    RISCVSuperSectionBits = 24
+};
+
+static inline unsigned int CONST
+pageBitsForSize(vm_page_size_t pagesize)
+{
+    switch (pagesize) {
+    case RISCVSmallPage:
+        return RISCVSmallPageBits;
+
+    case RISCVLargePage:
+        return RISCVLargePageBits;
+
+    case RISCVSection:
+        return RISCVSectionBits;
+
+    case RISCVSuperSection:
+        return RISCVSuperSectionBits;
+
+    default:
+        fail("Invalid page size");
+    }
+}
+
+#endif /* __ASSEMBLER__ */
+
+#endif /* !__ARCH_MACHINE_HARDWARE_H */
