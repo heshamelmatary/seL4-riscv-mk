16362fca0268b0a9b06b4819f4ffb2ccf60351a7
Hesham ALMatary <heshamelmatary@gmail.com>
2015-04-06 13:49:20 +0100

    Add IRQ stubs for qemu_riscv platform

--- src/plat/qemu_riscv/machine/hardware.c
+++ src/plat/qemu_riscv/machine/hardware.c
@@ -18,12 +18,6 @@
 #include <plat/machine/devices.h>
 #include <plat/machine/hardware.h>
 
-#define L2_LINE_SIZE_BITS 5
-#define L2_LINE_SIZE BIT(L2_LINE_SIZE_BITS)
-
-#define L2_LINE_START(a) ROUND_DOWN(a, L2_LINE_SIZE_BITS)
-#define L2_LINE_INDEX(a) (L2_LINE_START(a)>>L2_LINE_SIZE_BITS)
-
 /* Available physical memory regions on platform (RAM minus kernel image). */
 /* NOTE: Regions are not allowed to be adjacent! */
 
@@ -111,7 +105,7 @@ const p_region_t BOOT_RODATA dev_p_regs[] = {
 
 BOOT_CODE int get_num_dev_p_regs(void)
 {
-    return sizeof(dev_p_regs) / sizeof(p_region_t);
+  return 0;
 }
 
 BOOT_CODE p_region_t get_dev_p_reg(unsigned int i)
@@ -119,45 +113,19 @@ BOOT_CODE p_region_t get_dev_p_reg(unsigned int i)
     return dev_p_regs[i];
 }
 
-/* Memory map for AVIC (Advanced Vectored Interrupt Controller). */
-volatile struct avic_map {
-    uint32_t intctl;
-    uint32_t nimask;
-    uint32_t intennum;
-    uint32_t intdisnum;
-    uint32_t intenableh;
-    uint32_t intenablel;
-    uint32_t inttypeh;
-    uint32_t inttypel;
-    uint32_t nipriority[8];
-    uint32_t nivecsr;
-    uint32_t fivecsr;
-    uint32_t intsrch;
-    uint32_t intsrcl;
-    uint32_t intfrch;
-    uint32_t intfrcl;
-    uint32_t nipndh;
-    uint32_t nipndl;
-    uint32_t fipndh;
-    uint32_t fipndl;
-    uint32_t vector[64];
-} *avic = (volatile void *)AVIC_PPTR;
-
-/* Get the active IRQ number from the AVIC.  Returns 0xff if
- * there isn't one. Note this is also known as irqInvalid */
 /**
    DONT_TRANSLATE
  */
 interrupt_t
 getActiveIRQ(void)
 {
-    return (avic->nivecsr >> 16) & 0xff;
+  /* TODO */
+  return 0;
 }
 
 /* Check for pending IRQ */
 bool_t isIRQPending(void)
 {
-    return getActiveIRQ() != irqInvalid;
 }
 
 /* Enable or disable irq according to the 'disable' flag. */
@@ -167,11 +135,6 @@ bool_t isIRQPending(void)
 void
 maskInterrupt(bool_t disable, interrupt_t irq)
 {
-    if (disable) {
-        avic->intdisnum = irq;
-    } else {
-        avic->intennum = irq;
-    }
 }
 
 /* Determine if the given IRQ should be reserved by the kernel. */
@@ -185,7 +148,6 @@ isReservedIRQ(irq_t irq)
 void
 handleReservedIRQ(irq_t irq)
 {
-    printf("Received reserved IRQ: %d\n", (int)irq);
 }
 
 void
@@ -194,92 +156,9 @@ ackInterrupt(irq_t irq)
     /* empty on this platform */
 }
 
-/* Memory map for EPIT (Enhanced Periodic Interrupt Timer). */
-volatile struct epit_map {
-    uint32_t epitcr;
-    uint32_t epitsr;
-    uint32_t epitlr;
-    uint32_t epitcmpr;
-    uint32_t epitcnt;
-} *epit1 = (volatile void *)EPIT_PPTR;
-
-
-enum IPGConstants {
-    IPG_CLK = 1,
-    IPG_CLK_HIGHFREQ = 2,
-    IPG_CLK_32K = 3
-};
-
-#define TIMER_INTERVAL_MS (CONFIG_TIMER_TICK_MS)
-#define TIMER_CLOCK_SRC   IPG_CLK_32K
-#define TIMER_CLOCK_HZ    32768
-#define TIMER_RELOAD_VAL  (TIMER_CLOCK_HZ * TIMER_INTERVAL_MS / 1000)
-#if TIMER_RELOAD_VAL <= 0 || TIMER_RELOAD_VAL > 0xffffffff
-#error TIMER_RELOAD_VAL out of range
-#endif
-
-
-BOOT_CODE void
-map_kernel_devices(void)
-{
-    /* map kernel device: EPIT */
-    map_kernel_frame(
-        EPIT_PADDR,
-        EPIT_PPTR,
-        VMKernelOnly,
-        vm_attributes_new(
-            true,  /* armExecuteNever */
-            false, /* armParityEnabled */
-            false  /* armPageCacheable */
-        )
-    );
-
-    /* map kernel device: AVIC */
-    map_kernel_frame(
-        AVIC_PADDR,
-        AVIC_PPTR,
-        VMKernelOnly,
-        vm_attributes_new(
-            true,  /* armExecuteNever */
-            false, /* armParityEnabled */
-            false  /* armPageCacheable */
-        )
-    );
-
-    /* map kernel device: L2CC */
-    map_kernel_frame(
-        L2CC_PADDR,
-        L2CC_PPTR,
-        VMKernelOnly,
-        vm_attributes_new(
-            true,  /* armExecuteNever */
-            false, /* armParityEnabled */
-            false  /* armPageCacheable */
-        )
-    );
-
-#ifdef DEBUG
-    /* map kernel device: UART */
-    map_kernel_frame(
-        UART_PADDR,
-        UART_PPTR,
-        VMKernelOnly,
-        vm_attributes_new(
-            true,  /* armExecuteNever */
-            false, /* armParityEnabled */
-            false  /* armPageCacheable */
-        )
-    );
-#endif
-}
-
-/**
-   DONT_TRANSLATE
- */
 void
 resetTimer(void)
 {
-    epit1->epitsr = 1;
     /* Timer resets automatically */
 }
 
@@ -290,94 +169,25 @@ resetTimer(void)
 BOOT_CODE void
 initTimer(void)
 {
-    epitcr_t epitcr_kludge;
-
-    /* Stop timer */
-    epit1->epitcr = 0;
-
-    /* Configure timer */
-    epitcr_kludge.words[0] = 0; /* Zero struct */
-    epitcr_kludge = epitcr_set_clksrc(epitcr_kludge, TIMER_CLOCK_SRC);
-    /* Overwrite counter immediately on write */
-    epitcr_kludge = epitcr_set_iovw(epitcr_kludge, 1);
-    /* Reload from modulus register */
-    epitcr_kludge = epitcr_set_rld(epitcr_kludge, 1);
-    /* Enable interrupt */
-    epitcr_kludge = epitcr_set_ocien(epitcr_kludge, 1);
-    /* Count from modulus value on restart */
-    epitcr_kludge = epitcr_set_enmod(epitcr_kludge, 1);
-    epit1->epitcr = epitcr_kludge.words[0];
-
-    /* Set counter modulus */
-    epit1->epitlr = TIMER_RELOAD_VAL;
-
-    /* Interrupt at zero count */
-    epit1->epitcmpr = 0;
-
-    /* Clear pending interrupt */
-    epit1->epitsr = 1;
-
-    /* Enable timer */
-    epitcr_kludge = epitcr_set_en(epitcr_kludge, 1);
-    epit1->epitcr = epitcr_kludge.words[0];
 }
 
 static void invalidateL2(void)
 {
-    /* Invalidate all ways. */
-    imx31_l2cc_flush_regs->inv_by_way = 0xff;
-    /* Busy-wait for completion. */
-    while (imx31_l2cc_flush_regs->inv_by_way);
 }
 
 static void finaliseL2Op(void)
 {
-    /* We sync the l2 cache, which drains the write and eviction
-       buffers, to ensure that everything is consistent with RAM. */
-    imx31_l2cc_flush_regs->sync = 1;
 }
 
 void plat_cleanL2Range(paddr_t start, paddr_t end)
 {
-    paddr_t line;
-    word_t index;
-
-    for (index = L2_LINE_INDEX(start);
-            index < L2_LINE_INDEX(end) + 1;
-            index++) {
-        line = index << L2_LINE_SIZE_BITS;
-        imx31_l2cc_flush_regs->clean_by_pa = line;
-    }
-    finaliseL2Op();
 }
-
 void plat_invalidateL2Range(paddr_t start, paddr_t end)
 {
-    paddr_t line;
-    word_t index;
-
-    for (index = L2_LINE_INDEX(start);
-            index < L2_LINE_INDEX(end) + 1;
-            index++) {
-        line = index << L2_LINE_SIZE_BITS;
-        imx31_l2cc_flush_regs->inv_by_pa = line;
-    }
-
-    finaliseL2Op();
 }
 
 void plat_cleanInvalidateL2Range(paddr_t start, paddr_t end)
 {
-    paddr_t line;
-    word_t index;
-
-    for (index = L2_LINE_INDEX(start);
-            index < L2_LINE_INDEX(end) + 1;
-            index++) {
-        line = index << L2_LINE_SIZE_BITS;
-        imx31_l2cc_flush_regs->clinv_by_pa = line;
-    }
-    finaliseL2Op();
 }
 
 /**
@@ -386,16 +196,6 @@ void plat_cleanInvalidateL2Range(paddr_t start, paddr_t end)
 BOOT_CODE void
 initL2Cache(void)
 {
-#ifndef CONFIG_DEBUG_DISABLE_L2_CACHE
-    /* Configure L2 cache */
-    imx31_l2cc_ctrl_regs->aux_control = 0x0003001b;
-
-    /* Invalidate the L2 cache */
-    invalidateL2();
-
-    /* Enable the L2 cache */
-    imx31_l2cc_ctrl_regs->control = 1;
-#endif
 }
 
 /**
