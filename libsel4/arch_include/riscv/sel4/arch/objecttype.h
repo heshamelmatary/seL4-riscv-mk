8780c5beb28671938d10ea24be29808288d7c9c7
Hesham ALMatary <heshamelmatary@gmail.com>
2015-05-29 20:15:54 +0100

    Fix RISCV objecttypes

--- libsel4/arch_include/riscv/sel4/arch/objecttype.h
+++ libsel4/arch_include/riscv/sel4/arch/objecttype.h
@@ -12,16 +12,15 @@
 #define __ARCH_OBJECTTYPE_H
 
 typedef enum _object {
-    seL4_ARM_SmallPageObject = seL4_NonArchObjectTypeCount,
-    seL4_ARM_LargePageObject,
-    seL4_ARM_SectionObject,
-    seL4_ARM_SuperSectionObject,
-    seL4_ARM_PageTableObject,
-    seL4_ARM_PageDirectoryObject,
+    seL4_RISCV_4K = seL4_NonArchObjectTypeCount,
+    seL4_RISCV_LargePage,
+    seL4_RISCV_PageTableObject,
+    seL4_RISCV_PageDirectoryObject,
     seL4_ObjectTypeCount
 } seL4_ArchObjectType;
 
 typedef uint32_t object_t;
 
+#define seL4_RISCV_4M seL4_RISCV_LargePage
 #endif /* __ARCH_OBJECTTYPE_H */
 
