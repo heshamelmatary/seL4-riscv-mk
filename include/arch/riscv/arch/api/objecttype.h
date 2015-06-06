e6b7059d0517c616a1d7e797713f9976887f97bc
Hesham ALMatary <heshamelmatary@gmail.com>
2015-06-06 12:41:33 +0100

    Avoid naming conflict of seL4_RISCV_Page CPTR and the size type

--- include/arch/riscv/arch/api/objecttype.h
+++ include/arch/riscv/arch/api/objecttype.h
@@ -2,7 +2,7 @@
 #define __ARCH_OBJECT_TYPE_H
 
 typedef enum _object {
-    seL4_RISCV_Page = seL4_NonArchObjectTypeCount,
+    seL4_RISCV_PageObject = seL4_NonArchObjectTypeCount,
     seL4_RISCV_PageTableObject,
     seL4_RISCV_PageDirectoryObject,
     seL4_ObjectTypeCount
